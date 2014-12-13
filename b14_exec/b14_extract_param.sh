b1e83553a8fbd8cd4cb439743b2d7cec68601849
vineeth88 <vineeth88@users.noreply.github.com>
2014-12-12 19:15:37 -0500

    Some more cleanup

--- b14_exec/b14_extract_param.sh
+++
@@ -1,6 +0,0 @@
-
-# Finding number of branches hit [final]
-grep -A2 "Final" Ab14_$1_$2_512_* | grep "Branch"
-
-# Finding number of vectors in the test set
-grep -A1 "Final" Ab14_$1_$2_512_* | grep -o "210(.*)"
